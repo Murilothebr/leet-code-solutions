5694878d0997c075a578919f8702707a4b149f8c
murilothebr <murilothebr@hotmail.com>
2024-03-19 13:52:00 -0300

    1480. Running Sum of 1d Array

---
+++ 1480-Running Sum of 1d Array/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _1480._Running_Sum_of_1d_Array
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 1, 2, 10, 1 };
+
+            var newArr = RunningSum(nums);
+        }
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int i = 1;
+            while (i < nums.Length)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+                i += 1;
+            }
+
+            return nums;
+        }
+    }
+}
+ 
