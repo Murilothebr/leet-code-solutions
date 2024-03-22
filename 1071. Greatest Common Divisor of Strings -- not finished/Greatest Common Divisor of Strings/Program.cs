f5db46bc470e4f35dc7efc027364aedb4ea03513
murilothebr <murilothebr@hotmail.com>
2024-03-22 13:56:31 -0300

    add but not finished: 1071. Greatest Common Divisor of Strings -- not finished/

---
+++ 1071. Greatest Common Divisor of Strings -- not finished/Greatest Common Divisor of Strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Greatest_Common_Divisor_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var x = GcdOfStrings("ABABAB", "ABAB");
+            //var x = GcdOfStrings("ABCDEF", "ABC");
+            var x = GcdOfStrings("CTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXK", "CTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXKCTCXK");
+
+            Console.WriteLine(x);
+        }
+
+        public static string GcdOfStrings(string str1, string str2)
+        {
+            var GcdStg = "";
+
+            var smallerLenght = str1.Length;
+
+            if (str2.Length < str1.Length)
+                smallerLenght = str2.Length;
+
+            for (int i = 1; i <= smallerLenght; i++)
+            {
+                var slice1 = str1.Substring(0, i);
+                var slice2 = str2.Substring(0, i);
+
+                if (! slice1.Equals(slice2))
+                    continue;
+
+                if(! (isStringCompatible(str1: str1, str2: str2, index: i) && isStringCompatible(str1: str2, str2: slice1, index: i)))
+                    continue;
+
+                if (( str2.Contains(str1.Substring(i)) || str1.Contains(str2.Substring(i)) ) && ( str1.Contains(str2) || str2.Contains(str1) ))
+                    GcdStg = slice1;
+            }
+
+            return GcdStg;
+        }
+
+        public static bool isStringCompatible(string str1, string str2, int index) =>
+            str1.Length % str2.Substring(0, index).Length == 0;
+    }
+}
---
+++ 1431. Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Kids_With_the_Greatest_Number_of_Candies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 5, 1, 3 };
+
+            var x = KidsWithCandies(arr, 3);
+            Console.WriteLine(x);
+        }
+
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            var listIfGreatest = new List<bool>();
+            var mostAmoutOfCandies = 0;
+
+            foreach (var candieAmount in candies)
+            {
+                if (candieAmount > mostAmoutOfCandies)
+                    mostAmoutOfCandies = candieAmount;
+            }
+
+            foreach (var candieAmount in candies)
+                listIfGreatest.Add(candieAmount + extraCandies >= mostAmoutOfCandies);
+
+            return listIfGreatest;
+        }
+    }
+}
