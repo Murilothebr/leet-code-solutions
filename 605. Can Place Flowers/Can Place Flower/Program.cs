47427fba20a9c8fadda5d9697bcfd633b522dbc2
murilothebr <murilothebr>
2024-03-27 19:46:59 -0300

    605. Can Place Flowers

---
+++ 605. Can Place Flowers/Can Place Flower/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Can_Place_Flower;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = { 1, 0, 0, 0, 1 };
+
+        var y = CanPlaceFlowers(x, 2);
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        int canPlace = 0;
+
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if ((i == 0 || flowerbed[i - 1] == 0) && flowerbed[i] == 0 && (flowerbed[i + 1] == 0) || i == flowerbed.Length - 1)
+            {
+                flowerbed[i] = 1;
+                canPlace++;
+            }
+        }
+
+        return canPlace >= n;
+    }
+}
---
+++ 605. Can Place Flowers/Can Place Flowers/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Can_Place_Flowers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
