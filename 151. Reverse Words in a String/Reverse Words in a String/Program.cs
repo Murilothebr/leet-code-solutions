a3f87e31d0217c120ac7a59e8720c583134af018
murilothebr <murilothebr>
2024-03-27 22:18:11 -0300

    151. Reverse Words in a String -- 51 ms > 90% users, only 22% memory use (improve this)

---
+++ 151. Reverse Words in a String/Reverse Words in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Reverse_Words_in_a_String;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var x = ReverseWords("a good   example");
+
+
+        Console.WriteLine(x);
+    }
+
+    public static string ReverseWords(string s)
+    {
+        List<string> chunk = new List<string>();
+        string reverted = "";
+
+        foreach (string s2 in s.Split(" "))
+        {
+            if(s2 != "")
+                chunk.Add(s2);
+        }
+
+        for (int i = chunk.Count - 1; i >= 0; i--)
+        {
+            reverted += chunk[i] + " ";
+        }
+
+        return reverted.Trim();
+    }
+}
