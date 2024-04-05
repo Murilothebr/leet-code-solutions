4f8e7086ee55fd28180fff8221de41b742bb6351
murilothebr <murilothebr>
2024-04-05 10:38:33 -0300

    217. Contains Duplicate -- Horrible CPU usage, beats 80% memory users

---
+++ 217. Contains Duplicate/Contains Duplicate/Contains Duplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Contains_Duplicate;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 1 };
+
+        var x = ContainsDuplicate(nums);
+
+        Console.WriteLine(x);
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
