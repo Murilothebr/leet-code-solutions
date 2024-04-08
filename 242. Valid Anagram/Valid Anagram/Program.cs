cadbbecb5cf6db5bde8dd97cc2473d8f21c0928d
murilothebr <murilothebr>
2024-04-08 18:53:06 -0300

    242. Valid Anagram -- 70ms - beats 75% 43MB beats only 15% -- improve memory use

---
+++ 242. Valid Anagram/Valid Anagram/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = IsAnagram(s: "anagram", t: "nagaram");
+            Console.WriteLine(x);
+        }
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            return SortString(s) == SortString(t);
+        }
+
+        public static string SortString(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+    }
+}
