f725030cf30687ad5414cfa2214dda1fa7f064b1
murilothebr <murilothebr>
2024-04-09 11:06:54 -0300

    1. Two Sum: 120ms  beats 60% users -- 47mb beats 51% users

---
+++ 1. Two Sum/Two Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Two_Sum;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 3, 2, 4 };
+
+        var x = TwoSum(nums, 6);
+        Console.WriteLine(x);
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var arr = new int[2]{ 0 , 0 };
+
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        
+        return arr;
+    }
+}
