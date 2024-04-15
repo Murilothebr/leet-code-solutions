80750b7dc9366f122c678fa91ff539c030f0e936
murilothebr <murilothebr>
2024-04-15 14:40:17 -0300

    1. Two Sum 120 ms beats 60% users 47mb beats  51% users

--- 1. Two Sum/Two Sum/Program.cs
+++ 1. Two Sum/Two Sum/Program.cs
@@ -12,19 +12,15 @@ internal class Program
 
     public static int[] TwoSum(int[] nums, int target)
     {
-        var arr = new int[2]{ 0 , 0 };
-
         for (int i = 0; i < nums.Length; i++)
             for (int j = i + 1; j < nums.Length; j++)
             {
                 if (nums[i] + nums[j] == target)
                 {
-                    arr[0] = i;
-                    arr[1] = j;
-                    return arr;
+                    return new int[2] { i, j }; ;
                 }
             }
         
-        return arr;
+        return null;
     }
 }
