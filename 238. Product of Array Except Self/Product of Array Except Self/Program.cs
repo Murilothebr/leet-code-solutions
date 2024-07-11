00f638911b7e39286af14ed6385ffa9897eea15c
murilothebr <murilothebr>
2024-07-10 21:21:27 -0300

    arreguei

---
+++ 238. Product of Array Except Self/Product of Array Except Self/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Product_of_Array_Except_Self;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = { -1, 1, 0, -3, 3 };
+
+        var y = ProductExceptSelf(x);
+
+        Console.WriteLine("Hello, World!");
+    }
+    public static int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = result[i] * nums[i];
+        }
+        
+        return result;
+    }
+}
