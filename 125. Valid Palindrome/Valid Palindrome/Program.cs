61cf30c2246d3ed8c6e2463f2ac6f73278ed611a
Murilo Marcal <Murilo.Marcal@StoneX.com>
2024-07-27 12:07:12 -0300

    125. Valid Palindrome 59ms beats 75% of users, 47mb beats 20%

---
+++ 125. Valid Palindrome/Valid Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Two_Sum;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "race a car";
+
+        var a = IsPalindrome(s);
+
+        Console.WriteLine(a);
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        if(s.Equals(""))
+            return true;
+        
+        s = s.ToLower().Replace(" ", "");
+
+        s = Regex.Replace(s, @"[^a-zA-Z0-9]", "");
+
+        var regularChar = s.ToCharArray();
+        var inverse = regularChar.Reverse().ToArray();
+
+        return regularChar.SequenceEqual(inverse);
+    }
+}
+    
