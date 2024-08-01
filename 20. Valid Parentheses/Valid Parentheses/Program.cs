5fa3ba2de9d78fc57bd1232e2246eabf9272e426
murilothebr <murilothebr>
2024-08-01 09:19:32 -0300

    add

---
+++ 20. Valid Parentheses/Valid Parentheses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsValid("([]{})"));
+    }
+
+    public static bool IsValid(string s)
+    {
+        if (s.Length % 2 == 1)
+            return false;
+
+        var chars = s.ToCharArray();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if(i == s.Length -1)
+                continue;
+
+            if (chars[i + 1] == chars[i] + 1 || chars[i] == chars[i + 1] || chars[i + 2] == chars[i] + 2)
+            {
+                i++;
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+}
