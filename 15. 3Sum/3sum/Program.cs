1e98f8b1c14db97b1180de540917fa99abd7c244
murilothebr <murilothebr>
2024-08-14 09:37:28 -0300

    a

---
+++ 15. 3Sum/3sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Merge_Strings_Alternately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = ThreeSum([-1, 0, 1, 2, -1, -4]);
+            Console.WriteLine(teste);
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var list = new List<IList<int>>();
+
+            var numsList = nums.ToList();
+
+            int left = 0;
+            int right = 0; 
+            while (numsList.Count > 3)
+            {
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (numsList[0] + numsList[i] + numsList[i + 1] == 0)
+                    {
+                        list.Add(new
+                            List<int>([
+                                    numsList[0],
+                                    numsList[i],
+                                    numsList[i + 1]]));
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
